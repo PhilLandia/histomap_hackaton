8dfb586cf59631193e0c9a31d392c703219d78e5
qbic <golovinmax28@gmail.com>
2024-11-05 23:48:55 +0300

    refactor: rename class

--- src/main/java/com/example/histomap/entity/GeoJsonFeature.java
+++
@@ -1,7 +0,0 @@
-package com.example.histomap.entity;
-
-public class GeoJsonFeature extends AbstractGeoJsonEntity {
-    public GeoJsonFeature() {
-        this.type = "Feature";
-    }
-}
--- src/main/java/com/example/histomap/entity/Point.java
+++ src/main/java/com/example/histomap/entity/Point.java
@@ -1,10 +1,7 @@
 package com.example.histomap.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-
-public class Point extends GeoJsonFeature {
+public class Point extends GeoJsonFeatureImpl {
 
     public Point( float x, float y ) {
         this.geometry = new PointGeometry( x, y );
