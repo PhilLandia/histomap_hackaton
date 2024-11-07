3d31b9e181a67e2540bfedaf4c2117e56e97add6
qbic <golovinmax28@gmail.com>
2024-11-07 19:51:16 +0300

    feat: add enrichment logic

--- src/main/java/com/example/histomap/services/UserMapService.java
+++ src/main/java/com/example/histomap/services/UserMapService.java
@@ -1,9 +1,13 @@
 package com.example.histomap.services;
 
-import com.example.histomap.entity.MapMeta;
-import com.example.histomap.entity.MapName;
-import com.example.histomap.entity.UserMapEntity;
+import com.example.histomap.entity.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class UserMapService {
@@ -13,5 +17,34 @@ public class UserMapService {
 
     public MapMeta getMapMeta(UserMapEntity userMap) {
         return new MapMeta( userMap.getMeta().start(), userMap.getMeta().end(), userMap.getMeta().step() );
+    }
+
+    public ArrayList<ObjectNode> getGeoJsons(UserMapEntity userMap) {
+        ArrayList<ObjectNode> geoJsons = new ArrayList<>();
+
+
+
+        userMap.getData().forEach( userMapData -> {
+            ObjectNode geoJson = userMapData.getGeoJson();
+            GeoJsonProperty property = new GeoJsonProperty( userMap.getName(), userMapData.getYear() );
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode propertyNode = mapper.valueToTree( property );
+            geoJson.set( "property", propertyNode );
+
+            userMapData.getAnnotations().forEach( userMapDataAnnotation -> {
+                Point annotationPoint = new Point( userMapDataAnnotation.getCoordinates()[0], userMapDataAnnotation.getCoordinates()[1] );
+                HashMap<String, String> annotationProperty = new HashMap<>();
+                annotationProperty.put( "text", userMapDataAnnotation.getDescription() );
+                annotationPoint.setProperty( annotationProperty );
+                geoJsons.add( mapper.valueToTree( annotationPoint ) );
+            } );
+
+
+            geoJsons.add( geoJson );
+        } );
+
+        geoJsons.forEach(System.out::println);
+
+        return geoJsons;
     }
 }
